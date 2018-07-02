a7d231267c3285e8468ba0f1115d3bfaa70b37fa
marzique <marzique@gmail.com>
2018-07-03 01:33:36 +0300

    6th task contributions

---
+++ folder_monitor.py
@@ -0,0 +1,38 @@
+from time import sleep
+import os
+import configparser
+
+#
+def parse_settings(config_file):
+    config_settings = {}
+    # parse config file settings
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    for line in config['files']:
+        config_settings[line] =  config['files'][line]
+
+    return config_settings
+
+def file_action(filename):
+    print(filename)
+
+config_settings = parse_settings('config.ini')
+
+# go to desired directory
+cwd = os.getcwd() + "/Files"
+os.chdir(cwd)
+
+
+# check folder each second
+while True:
+    filenames = os.listdir()
+    for filename in filenames:
+        file_action(filename)
+    try:
+        sleep(1)
+    except:
+        # manual shutdown
+        print("==" * 10  + "finished monitoring" + "==" * 10)
+        break
+
