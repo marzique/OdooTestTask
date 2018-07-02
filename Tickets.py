83131802ffa649d18dce0d26f75f7df8fbf909de
marzique <marzique@gmail.com>
2018-07-02 22:38:38 +0300

    Done Task 3

--- Tickets.py
+++ Tickets.py
@@ -15,12 +15,12 @@ def calculate_combinations(combinations, N, Sum):
         for char2 in second_half:
             second_sum += int(char2)
 
-        if first_sum == second_sum == Sum:
+        if first_sum == second_sum == Sum/2:
             lucky_tickets.append(combination)
 
     return lucky_tickets
 
-# generate all possible combinations of N digits
+# generate all possible N digit combinations
 def generate_list(dimensions):
     combinations = []
     for i in itertools.product(range(10), repeat=dimensions):
@@ -28,21 +28,31 @@ def generate_list(dimensions):
         combinations.append(number)
     return combinations
 
-inputs = input("Enter input string:").split()
+def main():
+    inputs = input("Enter input string:").split()
 
-if inputs[0].isdigit() and inputs[1].isdigit():
+    # ensure valid input
+    if len(inputs) >= 2:
+        if inputs[0].isdigit() and inputs[1].isdigit():
 
-    N = int(inputs[0])
-    Sum = int(inputs[1])
-    if 1 <= N <= 50 and 1 <= Sum <+ 1000:
-        lis = generate_list(2*N)
-        lucky_combinations = calculate_combinations(lis, N, Sum)
-        print(len(lucky_combinations))
+            N = int(inputs[0])
+            Sum = int(inputs[1])
+            if 1 <= N <= 50 and 1 <= Sum <+ 1000:
+                lis = generate_list(2*N)
+                lucky_combinations = calculate_combinations(lis, N, Sum)
+                # print(lucky_combinations)
+                print(len(lucky_combinations))
+
+            else:
+                print("wrong input string")
+        else:
+            print("wrong input string")
 
     else:
         print("wrong input string")
 
-else:
-    print("wrong input string")
+
+if __name__ == "__main__":
+    main()
 
 
